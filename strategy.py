401df1315a22294329ed5e2048666002b1a64a79
terzim <ma.terzi@tiscali.it>
2016-08-27 15:06:25 +0100

    done work with correcting lists

--- strategy.py
+++ strategy.py
@@ -63,12 +63,12 @@ class RSIStrategy(object):
         if len(self.prices) == (self.min_window + 1):
             meangain = D(mean(self.gains))
             self.meangains.append(meangain)
-            return self.meangains[len(self.prices)-self.min_window-1]
+            return self.meangains[-1]
         
         elif len(self.prices) > (self.min_window + 1):
-            meangain = D(((self.meangains[len(self.prices)-self.min_window-2]*(self.min_window-1)) + self.gains[len(self.prices)-2])/(self.min_window))
+            meangain = D(((self.meangains[-1]*(self.min_window-1)) + self.gains[-1])/(self.min_window))
             self.meangains.append(meangain)
-            return self.meangains[len(self.prices)-self.min_window-1]
+            return self.meangains[-1]
 
     #computes the average loss given the string of midprices.
     #works the same as for the average gains
@@ -82,12 +82,12 @@ class RSIStrategy(object):
         if len(self.prices) == (self.min_window + 1):
             meanloss = D(mean(self.losses))
             self.meanlosses.append(meanloss)
-            return self.meanlosses[len(self.prices)-self.min_window-1]
+            return self.meanlosses[-1]
 
         elif len(self.prices) > (self.min_window + 1):
-            meanloss = D(((self.meanlosses[len(self.prices)-self.min_window-2]*(self.min_window-1)) + self.losses[len(self.prices)-2])/(self.min_window))
+            meanloss = D(((self.meanlosses[-1]*(self.min_window-1)) + self.losses[-1])/(self.min_window))
             self.meanlosses.append(meanloss)
-            return self.meanlosses[len(self.prices)-self.min_window-1]
+            return self.meanlosses[-1]
     
     #computes the Relative Strenght given the string of prices
     def RS(self):
@@ -103,23 +103,30 @@ class RSIStrategy(object):
     def print_signals(self,event):
         if len(self.prices) >= (self.min_window + 1):
             self.average_gain()
-            self.average_loss
+            self.average_loss()
             self.RSI()
-            print(meangains[-1])
-            print(meanlosses[-1])
-            print(rsilist[-1])        
+            print(self.meangains[-1])
+            print(self.meanlosses[-1])
+            print(self.rsilist[-1])        
 
     #TODO: this will provide the conditions under which the RSI will execute the order
     def calculate_signals(self, event):
-        if len(self.rsilist) > self.min_window:
-            halfminwin = int(self.min_window/2)
-            quarterminwin = int(halfminwin/2)
-            lastrsiwindow = self.rsilist[-halfminwin:]
-            if all(x > self.rsiupboundary for x in lastrsiwindow[0:quarterminwin]):
-                if all(x < self.rsiupboundary for x in lastrsiwindow[-quarterminwin:]):
-                    order = OrderEvent(self.instrument, self.units, "market", "sell")
+        if rsilist:
+            ordertype = "market"
+            orderdirection = ["sell","buy"]
+            #len(self.rsilist) > self.min_window:
+            #halfminwin = int(self.min_window/2)
+            #quarterminwin = int(halfminwin/2)
+            #lastrsiwindow = self.rsilist[-halfminwin:]
+            if all(x > self.rsiupboundary for x in rsilist[-10:-3]):
+                if all(x < self.rsiupboundary for x in rsilist[-3:]):
+                    order = OrderEvent(self.instrument, self.units, ordertype, orderdirection[0])
                     self.events.put(order)
-            elif all(x < self.rsilowboundary for x in lastrsiwindow[0:quarterminwin]):
-                if all(x > self.rsilowboundary for x in lastrsiwindow[-quarterminwin:]):
-                    order = OrderEvent(self.instrument, self.units, "market", "buy")
+            elif all(x < self.rsilowboundary for x in rsilist[-10:-3]):
+                if all(x > self.rsilowboundary for x in rsilist[-3:]):
+                    order = OrderEvent(self.instrument, self.units, ordertype, orderdirection[1])
                     self.events.put(order)
+            else:
+                pass
+        else:
+            pass
